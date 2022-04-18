38d42e1b9dbbd90fe2882f6babbe0e41a4e13bcd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-04-17 21:49:26 -0400

    Update DayTime.jl

--- src/DayTime.jl
+++ src/DayTime.jl
@@ -1,1 +1,12 @@
+struct DaysTime
+    days::Int64
+    time::Time
+end
+
+DaysTime(x::Time, y::Int) = DaysTime(y, x)
+DaysTime(x::Time) = DaysTime(0, x)
+DaysTime(x::Day) = DaysTime(value(x), Time0)
+DaysTime(x::Day, y::Time) = DaysTime(value(x), y)
+DaysTime(x::Time, y::Day) = DaysTime(value(y),  x)
+
 
