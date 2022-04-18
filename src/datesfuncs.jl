7999a07b24244a515ba96b1acc9d4a24246de838
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-04-17 21:43:28 -0400

    Update datesfuncs.jl

--- src/datesfuncs.jl
+++ src/datesfuncs.jl
@@ -1,2 +1,4 @@
-rata2date(x) = Date(rata2datetime(x))
-date2rata(x) = value(x)
+rata2date(x::Int) = Date(yearmonthday(x)...)
+rata2date(x::Day) = rata2date(value(x))
+  
+date2rata(x::Date) = value(x)
