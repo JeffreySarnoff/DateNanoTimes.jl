2eb5599ffab296aac6faf98ed0d750acb984c6b9
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-05-07 03:13:22 -0400

    Time Qualities and Quantities

---
+++ src/bitsfields/mythought.jl
@@ -0,0 +1,19 @@
+#=
+    Here are essential Date and Time Period types.  Now they are of
+    momentary interest.  When you return with a future perspective,
+    this interest will have extended the moment most effectively.
+
+    These essential types appear in order of increasing time spanned.
+    `Quarters` and `Weeks` are omitted for the purpose at hand,
+    as they both would overly other capable types' expression.
+    
+=#
+
+const TimeQualities = (
+    Nanosecond, Microsecond, Millisecond,
+    Second, Minute, Hour, Day, Month, Year)
+
+const TimeQuantities = (
+    nanosecond, microsecond, millisecond,
+    second, minute, hour, day, month, year)
+
