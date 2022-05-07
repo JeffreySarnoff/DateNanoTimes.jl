2bf84538f41adf6eb0775c4f5f9876671cbae449
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-05-07 03:30:48 -0400

    delcartion

--- src/DateNanoTimes.jl
+++ src/DateNanoTimes.jl
@@ -4,12 +4,13 @@ export DateNanoTimes
 
 using Dates
 
-include("bitsfields/mythought.jl")
-
 include("construction")
 include("constants")
-include("period_accessors.jl")
 
+include("bitsfields/mythought.jl")
+include("bitsfields/declaration.jl")
+
+include("period_accessors.jl")
 
 end # DateNanoTimes
 
