dc414cdfa8e04015d20e811d31951af63e781d17
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-05-07 02:41:42 -0400

    revisit

--- src/DayTime.jl
+++ src/DayTime.jl
@@ -5,7 +5,7 @@ end
 
 DaysTime(x::Time, y::Int) = DaysTime(y, x)
 DaysTime(x::Time) = DaysTime(0, x)
-DaysTime(x::Day) = DaysTime(value(x), Time0)
+DaysTime(x::Day)  = DaysTime(value(x), Time0)
 DaysTime(x::Day, y::Time) = DaysTime(value(x), y)
 DaysTime(x::Time, y::Day) = DaysTime(value(y),  x)
 
