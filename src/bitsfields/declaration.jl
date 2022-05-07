b5369b68a7cc1101aa3bb572c258ab177546db12
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-05-07 03:28:59 -0400

    decaration.jl

---
+++ src/bitsfields/declaration.jl
@@ -0,0 +1,9 @@
+#=
+    DatePeriods and TimePeriods are as fields for integer values.
+    When used this way, each has a predetermined maximum value.
+    During a day, no matter what the time, there will have
+    elapsed no more than 23 hours .. the transition into
+    the 24th hour carries one to first hour of the following day.
+
+
+=#
